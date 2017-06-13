37664b7f8aa24bfdf7a1a66ececd44d0c71711fb
jenskofodhansen <jens@kofod-hansen.com>
2017-06-13 21:38:49 +0200

    changes for settings

--- crosswordGA.py
+++ crosswordGA.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import random
 from builtins import filter
 from itertools import accumulate
@@ -28,7 +29,7 @@ gene_pool_size = 1000
 mutate_probability = 0.10 #chance of mutating a genome
 number_of_elite_chromosomes = int(gene_pool_size*0.01)
 number_of_new_chromosomes = int(gene_pool_size*0.2)
-number_of_epochs = 10000
+max_epochs = 10000
 
 # Specific parameters
 only_full_words = True
@@ -54,63 +55,63 @@ def create_random_chromosome():
 # Pretty print of the crossword chromosome
 def print_crossword(crossword):
     for idx in range(0, cross_width * cross_height, cross_width):
-        print(crossword[idx:idx + cross_width])
-     
+        print(''.join(crossword[idx:idx + cross_width]))
+
 
 # Calculate fitness value per word
 def get_word_fitness(word):
     max_fitness = 0
-    
+
     if word in vocabulary:
         return len(word)**long_word_multiplier
-    
+
     if (only_full_words):
         return 0
-    
+
     words_of_len = filter(lambda x: len(x) is len(word), vocabulary)
-    
+
     for voc_word in words_of_len:
         temp_fitness = sum(c1==c2 for c1,c2 in zip(word,voc_word))
-        
+
         max_fitness = max(max_fitness, temp_fitness)
-        
+
     return max_fitness**long_word_multiplier
-        
-        
+
+
 # Evaluate a single strings fitness
 def get_crossline_fitness(crossline):
     fitness = 0
-    
+
     line_as_string = ''.join(crossline)
-    
+
     # We take words from start until we hit either the end or the block character
     words = line_as_string.split(blockchar)
-    
+
     # For each word, they count if the match
     for word in words:
         if word in vocabulary:
-            fitness += get_word_fitness(word)    
-            
+            fitness += get_word_fitness(word)
+
     if line_as_string.find(blockchar*2) >= 0:
         return fitness / 2
-    
+
     return fitness
- 
- 
-# Evaluate the whole crossword       
+
+
+# Evaluate the whole crossword
 def get_crossword_fitness(crossword):
     fitness = 0
-    
-    # First we do row evaluation. 
+
+    # First we do row evaluation.
     for idx in range(0, cross_width * cross_height, cross_width):
         row = crossword[idx:idx + cross_width]
         fitness += get_crossline_fitness(row)
-        
+
     # Then we do column evaluation
     for idx in range(0, cross_width):
         column = crossword[idx:idx+(cross_height * cross_width):cross_width]
         fitness += get_crossline_fitness(column)
-    
+
     return fitness
 
 
@@ -119,99 +120,99 @@ def is_crossline_valid(crossline):
     # We take words from start until we hit either the end or the block character
     words = ''.join(crossline).split(blockchar)
     words = filter(lambda x: len(x)>0, words)
-    
+
     # For each word, they count if the match
     for word in words:
         if word not in vocabulary:
-            return False    
-            
+            return False
+
     return True
 
- 
-# Validate the whole crossword (are all words in the vocabulary?)      
+
+# Validate the whole crossword (are all words in the vocabulary?)
 def is_crossword_valid(crossword):
-    # First we do row evaluation. 
+    # First we do row evaluation.
     for idx in range(0, cross_width * cross_height, cross_width):
         row = crossword[idx:idx + cross_width]
         if is_crossline_valid(row) is False:
             return False
-        
+
     # Then we do column evaluation
     for idx in range(0, cross_width):
         column = crossword[idx:idx+(cross_height * cross_width):cross_width]
         if is_crossline_valid(column) is False:
             return False
-    
+
     return True
 
 
 # Breed a new chromosome by fusing two chromosomes at a randomly picked breakpoint. Apply a small chance of mutating the chromosome
 def pair_chromosomes(father, mother):
     breakpoint = random.randint(1, cross_width * cross_height)
-    
+
     father_gene = father[1]
     mother_gene = mother[1]
-    
+
     # 50:50 chance of pairing row-wise or column-wise
     if random.random()>=0.5:
         child = father_gene[:breakpoint] + mother_gene[breakpoint:]
     else:
         child = father_gene.copy()
-            
+
         for i in range(0, cross_width * cross_height):
             # convert to column-wise indexing
             ci = (i*cross_width)%(cross_width*cross_height) + i//cross_height
-            
+
             if i>=breakpoint: child[ci] = mother_gene[ci]
-    
+
     # Mutating the chromosome on genome level
     for i in range(0, cross_width * cross_height):
         if random.random() < mutate_probability:
             child[i] = pick_random_gene()
-            
+
     return child
 
 
 '''
 Pick a chromosome by fitness. This method uses roulette picking, where the chance of a chromosome
-being picked is equal to its fitness. 
+being picked is equal to its fitness.
 '''
 def pick_by_fitness(chromosomes_with_fitness, fitness_sum):
-    
+
     # quick picking
     idx = min(int(random.expovariate(8)*gene_pool_size), gene_pool_size-1)
     return chromosomes_with_fitness[idx]
-        
+
     # a random int between 0 and the total fitness
     roulette = random.randint(0, int(fitness_sum))
-    
-    ''' 
-    This line first maps only the fitness from the set of fitness and chromosome 
-    After this, it makes the accumulated fitness list using the itertools library
-    This accumulated list can be used to look up our chromosome picked by the roulette    
+
     '''
-    
+    This line first maps only the fitness from the set of fitness and chromosome
+    After this, it makes the accumulated fitness list using the itertools library
+    This accumulated list can be used to look up our chromosome picked by the roulette
+    '''
+
     # Unzip chromosomes with fitess
     fitness, chromosomes = zip(*chromosomes_with_fitness)
     accumulated_fitness = list(accumulate(fitness))
     idx = bisect(accumulated_fitness, roulette, 0, gene_pool_size-1)
-    
+
     return chromosomes_with_fitness[idx]
 
 
 # Save the result and settings used
 def save_settings_and_result(crossword, epoch):
     result_file = open("results.csv", "a+")
-    
+
     values = list()
     values.append(epoch)
-    
+
     # Solution found?
-    if epoch is number_of_epochs: 
-        values.append(False)  
-    else: 
+    if epoch == max_epochs:
+        values.append(False)
+    else:
         values.append(True)
-        
+
     values.append(cross_height)
     values.append(cross_width)
     values.append(blockchar_probablity)
@@ -231,7 +232,7 @@ def start_ga():
     chromosomes = [create_random_chromosome() for i in range(gene_pool_size)]
     
     # The actual GA process
-    for epoch in range(0, number_of_epochs):
+    for epoch in range(0, max_epochs):
         chromosome_fitness = [get_crossword_fitness(x) for x in chromosomes]
         
         # Make a list of sets containing the chromosome fitness, as well as the chromosome itself
@@ -293,14 +294,19 @@ def read_config():
     
     global mutate_probability 
     mutate_probability = ga_settings.getfloat("mutate_probability")
-    global number_of_elite_chromosomes 
-    number_of_elite_chromosomes = ga_settings.getint("number_of_elite_chromosomes")
-    global number_of_new_chromosomes 
-    number_of_new_chromosomes = ga_settings.getint("number_of_new_chromosomes")
-    global number_of_epochs 
-    number_of_epochs = ga_settings.getint("number_of_epochs")
-    
-    global long_word_multiplier 
+
+    global number_of_elite_chromosomes
+    percentage_of_elite_chromosomes = ga_settings.getfloat("percentage_of_elite_chromosomes")
+    number_of_elite_chromosomes = int(gene_pool_size*percentage_of_elite_chromosomes)
+
+    global number_of_new_chromosomes
+    percentage_of_new_chromosomes = ga_settings.getfloat("percentage_of_new_chromosomes")
+    number_of_new_chromosomes = int(gene_pool_size*percentage_of_new_chromosomes)
+
+    global max_epochs
+    max_epochs = ga_settings.getint("max_epochs")
+
+    global long_word_multiplier
     long_word_multiplier = ga_settings.getfloat("long_word_multiplier")
     
     global only_full_words
